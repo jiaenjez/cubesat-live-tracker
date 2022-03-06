35ee50143a4e9d7926a7d340d51638b31e72518f
jiaenjez <jiaenjez@gmail.com>
2022-03-06 01:40:30 -0600

    Change view defaults

--- src/mapbox/constants.ts
+++ src/mapbox/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-  public static DEFAULT_ZOOM = 7;
+  public static DEFAULT_ZOOM = 8;
   public static DEFAULT_LATITUDE = 33.6405;
   public static DEFAULT_LONGITUDE = -117.8443;
 }
